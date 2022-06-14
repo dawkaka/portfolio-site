132388fe04e93fd0088934ebf27ea33057ed592a
Yousiph1 <yousiph77@gmail.com>
2022-06-14 10:20:05 +0000

    se fini

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,8 +3,11 @@ import {FunctionComponent, CSSProperties} from 'react'
 
 import styles from '../styles/Home.module.css'
 
-const NavItem: FunctionComponent<{title: string; styleColor: string}> = ({title, styleColor}) => {
+const NavItem: FunctionComponent<{title: string; styleColor: string,delay:string}> = ({title, styleColor,delay}) => {
   const handleClick = () => {
+    const container = document.querySelector(`.${styles.navLinksContainer}`)
+    if(!container) return
+    container.classList.remove(`${styles.smallScreenNav}`)
     const sec = document.getElementById(title.toLowerCase())
     if(!sec) return
     sec.scrollIntoView()
@@ -12,7 +15,7 @@ const NavItem: FunctionComponent<{title: string; styleColor: string}> = ({title,
   return (
      <li onClick={handleClick}
      className={`${styles.navLink} ${styles.textNormal}`}
-     style={{'--styleColor': styleColor} as CSSProperties}
+     style={{'--styleColor': styleColor,'--delay': delay} as CSSProperties}
      >
      {title}.
      </li>
@@ -20,6 +23,25 @@ const NavItem: FunctionComponent<{title: string; styleColor: string}> = ({title,
 }
 
 const NavBar: FunctionComponent = () => {
+
+  const showSmallScreenMenu = () => {
+
+    const container = document.querySelector(`.${styles.navLinksContainer}`)
+    const lines = Array.from(document.querySelectorAll(`.${styles.humLine}`))
+    if(!container) return
+    if(Array.from(container.classList).includes(`${styles.smallScreenNav}`)) {
+      container.classList.remove(`${styles.smallScreenNav}`)
+      lines[0].classList.remove(`${styles.humLine1}`)
+      lines[1].classList.remove(`${styles.humLine2}`)
+    }else {
+      container.classList.add(`${styles.smallScreenNav}`)
+      lines[0].classList.add(`${styles.humLine1}`)
+      lines[1].classList.add(`${styles.humLine2}`)
+    }
+
+
+
+  }
 
    return(
      <div className={styles.heroMain}>
@@ -34,6 +56,10 @@ const NavBar: FunctionComponent = () => {
      <NavItem title="Projects" styleColor ="var(--pink)" />
      <NavItem title="Education" styleColor ="var(--yellow)" />
      </ul>
+     <div className={styles.humburgerContainer} onClick={showSmallScreenMenu}>
+     <div className={styles.humLine}></div>
+     <div className={styles.humLine}></div>
+     </div>
      </nav>
      </header>
      </div>
